cc45cfcf18261e08fe134ad60703ffed757b34dd
dagdemir-96 <pnrdagdemir1@gmil.com>
2025-04-08 23:42:37 +0300

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ogrencibir
 # ogrencibir
 # ogrencibir
+# Cook-Site
--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ searchBtn.addEventListener("click", function(){
     searchForm.classList.toggle("active");  
     document.addEventListener("click",function(e){
         if(
-            !e.composedPath().includes(searchBtn) && !e.composedPath().includes(searchForm)
+            !e.composedPath().includes(searchBtn) && 
+            !e.composedPath().includes(searchForm)
         ){
             searchBtn.classList.remove("active");
         }
@@ -24,7 +25,8 @@ cartBtn.addEventListener("click", function(){
     cartItem.classList.toggle("active");  
     document.addEventListener("click",function(e){
         if(
-            !e.composedPath().includes(cartBtn) && !e.composedPath().includes(cartItem)
+            !e.composedPath().includes(cartBtn) && 
+            !e.composedPath().includes(cartItem)
         ) {
             cartItem.classList.remove("active");
         }
@@ -35,7 +37,8 @@ menuBtn.addEventListener("click", function(){
     navbar.classList.toggle("active");  
     document.addEventListener("click",function(e){
         if(
-            !e.composedPath().includes(menuBtn) && !e.composedPath().includes(navbar)
+            !e.composedPath().includes(menuBtn) && 
+            !e.composedPath().includes(navbar)
         ) {
             navbar.classList.remove("active");
         }
